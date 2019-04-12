fbeaf22971aee097c16c692cc1023a152d6060a7
Animesh <ansa0972@colorado.edu>
2019-04-12 12:12:34 -0600

    recent commit

--- selenium_test.py
+++ selenium_test.py
@@ -1,17 +1,24 @@
-import time
+import unittest2
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Firefox()
-time.sleep(30)
+class GoogleSearchTest(unittest2.TestCase):
 
-browser.get('http://www.google.com')
-WebDriverWait(driver, 10)
-search = browser.find_element_by_name('q')
-search.send_keys("google search through python")
-search.send_keys(Keys.RETURN) # hit return after you enter search text
-time.sleep(50) # sleep for 5 seconds so you can see the results
-browser.quit()
+    def setUp(self):
+        # Create a new Firefox driver instance
+        self.driver = webdriver.Firefox()
+
+    def tearDown(self):
+        # Close the browser after running the tests
+        self.driver.quit()
+
+    def testSearch(self):
+        # Trigger a Google Search
+        self.driver.get('http://www.google.com')
+        self.assertEqual(self.driver.title, 'Google')
+        searchElement = self.driver.find_element_by_name('q')
+        searchElement.send_keys('Animesh Kumar Sahu')
+        searchElement.submit()
+        WebDriverWait(self.driver, 30).until(EC.title_contains('Animesh Kumar Sahu'))
+        self.assertTrue(self.driver.title.startswith('Animesh Kumar Sahu'))
