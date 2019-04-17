f7b590ec8923b7a1edd319c698846063f91c799f
animax2612 <49383545+animax2612@users.noreply.github.com>
2019-04-17 12:07:03 -0600

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -1,5 +1,5 @@
 import unittest2 
-from selenium import webdriver
+from selenium import webdriver 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
